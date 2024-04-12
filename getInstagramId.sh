dce9c562fbb20e73fb962f0281b1c364338334b3
cradle8810 <falcon.int50m@gmail.com>
2024-04-12 14:06:10 +0900

    Add Download method
    But not implimented archiving

--- getInstagramId.sh
+++ getInstagramId.sh
@@ -3,19 +3,70 @@
 POCKETJSON="pocket_unread.json"
 INSTALOADER_PATH="${HOME}/.local/bin/instaloader"
 
-if [ ! -f "{INSTALOADER_PATH}" ]; then
+if [ ! -x "${INSTALOADER_PATH}" ]; then
     echo "No instaloader in ${INSTALOADER_PATH}" > /dev/stderr
     exit 1
 fi
-
 
 if [ -f "${POCKETJSON}" ]; then
     grep 'instagram' < pocket_unread.json | \
 	grep "resolved_url" | \
 	sort | \
 	awk -F '/' '{print $5}' > list
+else
+    echo "No ${POCKETJSON}." > /dev/stderr
+    exit 2
 fi
 
-xargs -I{} -P10 -n1 -t \
-      "${INSTALOADER_PATH}" --filename-pattern '{filename}' -- "-{}" < list
+# make Image Dir
+IMAGEDIR="images"
+mkdir "${IMAGEDIR}"
+if [ ! -d "${IMAGEDIR}/" ]; then
+    echo "Can't makedir ${IMAGEDIR}. halt." > /dev/stderr
+    exit 3
+fi
 
+# make Temporary Dirs
+TEMPDIR="temp"
+mkdir "${TEMPDIR}"
+if [ ! -d "${TEMPDIR}/" ]; then
+    echo "Can't makedir ${TEMPDIR}. halt." > /dev/stderr
+    exit 4
+fi
+
+# make download target instagram article IDs from pocket URL
+grep 'instagram' < "${POCKETJSON}" | \
+    grep "resolved_url" | \
+    sort | awk -F '/' '{print $5}' > list
+
+LISTCOUNT=$(wc -l < list)
+if [ "${LISTCOUNT}" -le 0 ]; then
+    echo "No instagram URL found."  > /dev/stderr
+    exit 0
+fi
+echo "There are ${LISTCOUNT} URLs found."
+
+# Download Image into images/ dir
+DIRNAME_PATTERN="${TEMPDIR}/{target}"
+while read id; do
+echo "Downloading ${id}."
+"${INSTALOADER_PATH}" \
+    --dirname-pattern "${DIRNAME_PATTERN}" \
+    --filename-pattern '{filename}' -- \
+    "-${id}"
+done < list
+
+# Get Pocket Article ID from pocket JSON
+xargs -I{} -n1 grep -B 14 {} "${POCKETJSON}" < list | \
+    grep 'item_id' | awk -F: '{print $2}' | \
+    tr -d '", ' > remove_pocket_id.txt
+
+# Move Photo and Texts on the directory into imagedir/
+find "${TEMPDIR}/" -type f -exec mv {} ${IMAGEDIR}/ \;
+
+# Remove Wastes.
+rm -fr "${IMAGEDIR}/*.xz" "${IMAGEDIR}/*.txt"
+
+# Make Archives
+DATE=$(date +%Y%m%d)
+zip "Instagram_${DATE}.zip" "${IMAGEDIR}/"
